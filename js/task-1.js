13ae3a971e16ba610e65ba03f3ceca46b2e0de31
Vitaliy-67 <125982107+Vitaliy-67@users.noreply.github.com>
2024-09-19 16:52:57 +0300

    commit message

--- js/task-1.js
+++ js/task-1.js
@@ -1,25 +1,18 @@
 function isEnoughCapacity(products, containerSize) {
-    const fruits = products;
-    console.log(fruits);
-    
-  
+  let fruitBasket = 0;
+  const fruits = {
+    ...products,
   };
-  
-  
-  
-  console.log(
-    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-  ); // false
-  
-  console.log(
-    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-  ); // false
-  
+  for (const key in fruits) {
+    fruitBasket += fruits[key];
+  }
+  return fruitBasket <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
